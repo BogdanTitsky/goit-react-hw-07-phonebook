183ea55bc12f34e152393091efa461597387680c
unknown <bogdan.titsky@gmail.com>
2023-07-11 14:37:18 +0300

    optimized

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,10 +3,18 @@ import { Filter } from './Filter/Filter';
 import { ContactForm } from './ContactForm/ContactForm';
 import { useSelector } from 'react-redux';
 import { selectError, selectIsLoading } from 'redux/selectors';
+import { fetchContacts } from 'redux/operations';
+import { useEffect } from 'react';
+import { useDispatch } from 'react-redux';
 
 export const App = () => {
   const isLoading = useSelector(selectIsLoading);
   const error = useSelector(selectError);
+
+  const dispatch = useDispatch();
+  useEffect(() => {
+    dispatch(fetchContacts());
+  }, [dispatch]);
 
   return (
     <div className="app">
--- src/components/ContactList/ContactList.jsx
+++ src/components/ContactList/ContactList.jsx
@@ -1,22 +1,9 @@
 import { useSelector } from 'react-redux';
 import { ContactItem } from './ContactItem/ContactItem';
-import { selectContacts, selectFilter } from 'redux/selectors';
-import { fetchContacts } from 'redux/operations';
-import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { selectFilteredContacts } from 'redux/selectors';
+
 export const ContactList = () => {
-  const contacts = useSelector(selectContacts);
-  
-  const dispatch = useDispatch();
-  useEffect(() => {
-    dispatch(fetchContacts());
-  }, [dispatch]);
-
-  const filter = useSelector(selectFilter);
-  const filteredContacts = contacts.filter(contact =>
-    contact.name.toLowerCase().includes(filter.toLowerCase())
-  );
-
+  const filteredContacts = useSelector(selectFilteredContacts);
   return (
     <ul>
       {filteredContacts.map(contact => (
